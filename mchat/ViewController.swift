4f7d35597c238bc13ac9a2bfe4526d780dc9f6d0
lotawei <lotawei@qq.com>
2019-03-29 23:49:34 -0700

    Initial Commit

---
+++ mchat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mchat
+//
+//  Created by lotawei on 2019/3/29.
+//  Copyright © 2019 lotawei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
